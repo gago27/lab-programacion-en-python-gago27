7c5a6926456e53b9667d175b61fe04e8af5769c6
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-21 10:42:16 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -3,13 +3,12 @@
 ##
 import glob
 filenames = glob.glob("data.csv")
-data = open("data.csv", "r").readlines()
+data = open("data.csv","r").readlines()
 data = [line[:-1] for line in data]
 data = [line.replace("\t", ",") for line in data]
 data = [line.split(",") for line in data]
 data = [line[1] for line in data]
 a = 0
-for i in data:
-  a = a+int(i)
-a
-python3  __grade__.py
+for row in data:
+  a= a+ int(row)
+print(a)
