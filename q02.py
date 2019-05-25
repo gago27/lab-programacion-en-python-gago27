6dd19db89d4b96262294f143e730951e7e907881
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-24 23:18:35 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -8,3 +8,22 @@
 ## D,6
 ## E,14
 ##
+import glob
+filenames = glob.glob("data.csv")
+data = open("data.csv","r").readlines()
+data = [line[:-1] for line in data]
+data = [line.replace("\t", ",") for line in data]
+data = [line.split(",") for line in data]
+data = [line[0] for line in data]
+datas = set([line[0] for line in data])
+b = []
+for i in datas:
+  a = 0
+  for m in data:
+    if i == m:
+      a = a+1
+  b.append((i,a))
+b = sorted(b)
+for h in b:
+  resp= h[0]+","+str(h[1])
+  print(resp)
