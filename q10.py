8db4b2e9b1206d2986876abf7790f0283aa69728
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-26 11:57:31 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -15,3 +15,33 @@
 ## jjj,18
 ##
 ##
+import glob
+import csv 
+filenames = glob.glob("data.csv")
+data = open("data.csv", "r").readlines()
+data = [line[:-1] for line in data]
+data = [line.replace("\t",",") for line in data]
+data = [line.split(",") for line in data]
+data = [line[5:] for line in data]
+data = [[row for row in line if len(row)==5] for line in data]
+data = [[row[0:3] for row in line] for line in data]
+datas = []
+for line in data:
+  for row in line:
+    datas.append(row)
+datas = sorted(datas)
+datas0 = set(datas)
+datas0 = [line for line in datas0]
+datas0 = sorted(datas0)
+tabla=[]
+for a in datas0:
+  b = datas.count(a)
+  fila=[a,b]
+  tabla= tabla+[fila]
+
+file2 = glob.glob("data1.csv*")
+data1 = open("data1.csv", "w")
+x = csv.writer(data1, delimiter=",",quoting=csv.QUOTE_NONNUMERIC)
+for i in range(len(tabla)):
+  x.writerow(tabla[i])
+print(open("data1.csv","r").read())
