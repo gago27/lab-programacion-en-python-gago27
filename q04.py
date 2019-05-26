5b4bce5fed807174762a89775684ed06f1c815f2
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-26 11:51:24 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -14,3 +14,24 @@
 ## 11,2
 ## 12,3
 ##
+import glob
+filenames = glob.glob("data.csv")
+data = open("data.csv","r").readlines()
+data = [line[:-1] for line in data]
+data = [line.replace("\t",",") for line in data]
+data = [line.split(",") for line in data]
+datas = [line[2] for line in data]
+datas = sorted([dates[5:7] for dates in datas])
+datas0 = set(datas)
+h = []
+for i in datas0:
+  j = [i]
+  a = 0
+  for m in datas:
+    if m == i:
+      a = a+1
+  j= j+[a]
+  h = sorted(h+[j])
+for q in h:
+  resp=str(q[0])+","+str(q[1])
+  print(resp)
