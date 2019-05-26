99433604509946421f92667a2b2b9aee738c60fa
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-26 11:59:41 -0500

    Update q13.py

--- q13.py
+++ q13.py
@@ -10,3 +10,23 @@
 ## E,11
 ## E,16
 ##
+import glob
+filenames= glob.glob("data.csv")
+data = open("data.csv","r").readlines()
+data = [line[:-1] for line in data]
+data = [line.replace("\t",",") for line in data]
+data = [line.split(",") for line in data]
+letras = [line[0] for line in data]
+columna = [line[5:] for line in data]
+columna = [[row for row in line if len(row)==5] for line in columna]
+columna = [[int(row[4]) for row in line] for line in columna]
+numeros=[]
+for row in columna:
+  a=0
+  for num in row:
+    a = a+num
+  numeros.append(a)
+numeros
+for i in range(len(numeros)):
+  resp= str(letras[i])+","+str(numeros[i])
+  print(resp)
