b550fcdbc8cd2bb40c3303fce203c95e898dfa1c
Santiago Ramirez <50551845+gago27@users.noreply.github.com>
2019-05-26 11:54:11 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -13,3 +13,41 @@
 ## iii,2,7
 ## jjj,2,5
 ##
+import glob
+filenames=glob.glob("data.csv")
+data = open("data.csv", "r").readlines()
+data = [line[:-1] for line in data]
+data = [line.replace("\t", ",") for line in data]
+data = [line.split(",") for line in data]
+data = [line[5:] for line in data]
+datas=[]
+for line in data:
+  row = []
+  for x in line:
+    if len(x)==5:
+      row.append(x)
+  datas.append(row)
+datas0 = []
+for i in datas:
+  for a in i:
+    datas0.append(a)
+datas0 = sorted(datas0)
+datas1 = []
+datas2=[line.replace(":",",") for line in datas0]
+datas2=[line.split(",") for line in datas2]
+otro = set([line[0]for line in datas2])
+nuevo = []
+for x in otro:
+  let=[x]
+  for este in datas2:
+    if este[0]==x:
+      let.append(este[1])
+  nuevo= nuevo+[let]
+an= []
+for fila in nuevo:
+  casi = [fila[0], min(fila), max(fila[1:])]
+  an= an + [casi]
+an = sorted(an) 
+for y in an:
+  resp= y[0]+","+y[1]+","+y[2]
+  print(resp)
